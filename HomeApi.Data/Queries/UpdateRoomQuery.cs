1ef27b221e0e9937cbbc33122d4bb8a1420ac68b
Дмитрий Санкеев <d.sankeev@gmail.com>
2024-02-15 00:39:52 +0300

    Add project files.

---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeApi.Data.Queries
+{
+    /// <summary>
+    /// Класс для передачи дополнительных параметров при обновлении комнаты
+    /// </summary>
+    public class UpdateRoomQuery
+    {
+        public string NewName { get; } 
+        public int NewArea { get; }       
+        public bool NewGasConnectionState { get; }
+        public int NewVoltage { get; }       
+
+        public UpdateRoomQuery( string newName = null, int newArea = 0, bool newGasConnectionState = false, int newVoltage = 0)
+        {
+            NewName = newName;
+            NewArea = newArea;
+            NewGasConnectionState = newGasConnectionState;
+            NewVoltage = newVoltage;          
+            
+        }
+    }
+}
